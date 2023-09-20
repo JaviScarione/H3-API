a7e466d6f1e32258b28c0011e2c68e715b2a7409
Javier <javierscarione@gmail.com>
2023-09-20 15:14:43 -0300

    listClients

--- H3-Front/src/pages/ListClients/ListClients.jsx
+++ H3-Front/src/pages/ListClients/ListClients.jsx
@@ -4,27 +4,31 @@ import Footer from '../../components/Footer/Footer'
 import AnimatedPages from "../AnimatedPages/AnimatedPages";
 import { useClient } from '../../context/ClientContext';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { MdEditSquare } from "react-icons/md";
+
 
 
 
 function ListClients () {
 
-    const { getClients } = useClient();
+    const { getClients, clients } = useClient();
 
     useEffect(() => {
         getClients()
     }, []);
 
-    
 
     return (
         <div className='listClients'>
             <Navbar />
                 <AnimatedPages>
                     <h1><u>Consultar Clientes</u></h1>
+                    <div className="tableContainer">
                     <table>
                         <thead>
                         <tr>
+                            <th></th>
                             <th>Nombre</th>
                             <th>Dirección</th>
                             <th>Teléfono</th>
@@ -39,19 +43,31 @@ function ListClients () {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>Dato 1</td>
-                            <td>Dato 2</td>
-                            <td>Dato 3</td>
-                        </tr>
-                        <tr>
-                            <td>Dato 4</td>
-                            <td>Dato 5</td>
-                            <td>Dato 6</td>
-                        </tr>
-                        
+                        { clients.map( item => {
+                            const client = item.client;
+                            console.log(item.client);
+                            return (
+                                <tr key={client._id}>
+                                    <td><Link to={`/modifyclient`} className='editClient'><MdEditSquare /></Link></td>
+                                    <td>{client.name}</td>
+                                    <td>{client.address}</td>
+                                    <td>{client.phone}</td>
+                                    <td>{client.cuit}</td>
+                                    <td>{client.day}</td>
+                                    <td>{item.h320}</td>                                    
+                                    <td>{item.h312}</td>                                    
+                                    <td>{item.lgm}</td>                                    
+                                    <td>{item.lgbs}</td>                                    
+                                    <td>{item.soda}</td>                                    
+                                    <td>{item.fc}</td>                                    
+                                </tr>
+                            )
+                        })                        
+                        }                        
                         </tbody>
                     </table>
+                    </div>
+                    <Link to='/home'><button className='cancelConsultClients'>Volver</button></Link>
                 </AnimatedPages>
             <Footer />
         </div>
